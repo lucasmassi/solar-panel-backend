16fdadb048fcd99cc06303302df4356a5f85fed5
Lucas Massi <lucasmassi18@gmail.com>
2019-09-19 18:54:59 -0300

    Add Cors and configs env

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import './bootstrap';
 import express from 'express';
 import path from 'path';
 import Youch from 'youch';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -18,6 +19,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
 
     this.server.use(
