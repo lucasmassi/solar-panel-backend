112a1ec4c8c606e386fd5e1f34c77b833de151c9
Lucas Massi <lucasmassi18@gmail.com>
2019-09-19 22:19:06 -0300

    Remove paginate in panels

--- src/app/controllers/PanelController.js
+++ src/app/controllers/PanelController.js
@@ -3,15 +3,11 @@ import User from '../models/User';
 
 class PanelController {
   async index(req, res) {
-    const { page = 1 } = req.query;
-
     const user = await User.findByPk(req.userId);
 
     const panels = await Panel.findAll({
       where: { state: user.state },
       order: ['installation_date'],
-      limit: 5,
-      offset: (page - 1) * 5,
       attributes: [
         'id',
         'installation_date',
